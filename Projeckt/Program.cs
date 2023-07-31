0f2d8d8ee420d3a5a2005ed0751cd4df81e97349
Grandhald <kel_tua@mail.ru>
2023-07-31 18:40:03 +0300

    working programm

---
+++ Projeckt/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше,
+// либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string InputWords(string message)
+{
+    Console.WriteLine(message);
+    string word = Console.ReadLine();
+    return word;
+}
+
+string[] CreateArray(string message)
+{
+    Console.WriteLine(message);
+    int count = Convert.ToInt32(Console.ReadLine());
+    string[] arrayWords = new string[count];
+    for (int i = 0; i < arrayWords.Length; i++)
+    {
+        arrayWords[i] = InputWords("Введите строку: ");
+    }
+    return arrayWords;
+}
+
+string[] CreateArrayLenWordsAreLessThenTree(string[] words)
+{
+    string strWords = "";
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= 3)
+        {
+            strWords += words[i]+" ";
+        }
+    }
+    strWords = strWords.Substring(0, strWords.Length-1);
+    string[] arrayLenWordsAreLessThenTree = strWords.Split(' ');
+    return arrayLenWordsAreLessThenTree;
+}
+
+
+string[] arrayWords = CreateArray("Введите количество строк: ");
+string[] arrayWordlenLessTheree = CreateArrayLenWordsAreLessThenTree(arrayWords);
+Console.Write($"[{string.Join(", ", arrayWords)}] --> [{string.Join(", ", arrayWordlenLessTheree)}]");
